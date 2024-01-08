f6d0a95d05af2d96abdd3ea2b6c60c5b3e7412fd
Dari <pinobobino@gmail.com>
2024-01-08 16:46:20 +0300

    Вывод товаров

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Items from './components/Items/Items'
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpeg',
+          desc: 'lolkek',
+          category: 'chairs',
+          price: '49,99 $',
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.webp',
+          desc: 'lolkek',
+          category: 'tables',
+          price: '149,00 $',
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'lolkek',
+          category: 'chairs',
+          price: '549,99$',
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'wall-light.jpeg',
+          desc: 'lolkek',
+          category: 'chairs',
+          price: '25,00$',
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpeg',
+          desc: 'lolkek',
+          category: 'chairs',
+          price: '49,99$',
+        },
+      ],
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/components/Items/Items.js
@@ -0,0 +1,17 @@
+import './Items.css'
+import React, { Component } from 'react'
+import Item from '../Item/Item'
+
+export class Items extends Component {
+  render() {
+    return (
+      <main>
+        {this.props.items.map((el) => (
+          <Item key={el.id} item={el} />
+        ))}
+      </main>
+    )
+  }
+}
+
+export default Items
